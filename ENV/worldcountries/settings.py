572d7c0ad942998910372cacf17804c4172b0885
ottojoash <louisjoshbricks@gmail.com>
2023-03-20 22:35:55 +0300

    settings

--- ENV/worldcountries/settings.py
+++ ENV/worldcountries/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.CorsMiddleware'
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',

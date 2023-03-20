90a54da649ea439013fda56cde96420e386d2c4a
ottojoash <louisjoshbricks@gmail.com>
2023-03-20 23:48:59 +0300

    models

--- ENV/countries/models.py
+++ ENV/countries/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Countries(models.Model):
+    name = models.CharField(max_length=50,blank=False,default='')
+    capital = models.CharField(max_length=50,blank=False,default='')

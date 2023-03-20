316d9a76b3d829062ca1d88f8a8003cad284fedc
ottojoash <louisjoshbricks@gmail.com>
2023-03-20 23:52:39 +0300

    models

--- ENV/countries/models.py
+++ ENV/countries/models.py
@@ -6,3 +6,6 @@ from django.db import models
 class Countries(models.Model):
     name = models.CharField(max_length=50,blank=False,default='')
     capital = models.CharField(max_length=50,blank=False,default='')
+
+    class Meta:
+        ordering = ('id',)
